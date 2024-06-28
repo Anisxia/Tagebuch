3896d72d3339023854ea702237e2823fc4267f5a
AnissaSi <92747266+AnissaSi@users.noreply.github.com>
2024-06-28 16:02:33 +0200

    hghg

---
+++ Tagebuch/main.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+        const el = this.el;
+        el.addEventListener('click', function (evt) {
+            const action = el.getAttribute('cursor-listener').action;
+            if (action === 'changeColor') {
+                el.setAttribute('color', '#FF0000'); // Beispielaktion: Farbe ändern
+            } else if (action === 'nextPage') {
+                // Beispielaktion: Zur nächsten Seite wechseln (hier implementieren, was als nächstes passieren soll)
+                console.log('Zur nächsten Seite wechseln');
+                // Hier können Sie beispielsweise den Szenenwechsel implementieren
+                window.location.href = 'nextpage.html'; // Beispiel: Weiterleitung zu einer nächsten HTML-Seite
+            }
+        });
+    }
+});
